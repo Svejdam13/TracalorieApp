c6446f673427878f83488b5656e157cbad74c702
Martin Svejda <svejdam13@gmail.com>
2020-08-15 22:47:31 +0100

    update

--- app.js
+++ app.js
@@ -24,7 +24,19 @@
       return data.items;
     },
     addItem: function(name, calories){
-      
+      let ID;
+      // Create ID
+      if(data.items.length > 0){
+        ID = data.items[data.items.length-1].id +1;
+      } else {
+        ID = 0;
+      } 
+      // Calories to number
+      calories = parseInt(calories);
+      // Create new item
+      newItem = new Item(ID, name, calories);
+      // Add to items array
+      data.items.push(newItem);
     },
     logData: function(){
       return data;
